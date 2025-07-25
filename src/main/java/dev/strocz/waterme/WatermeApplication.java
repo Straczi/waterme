d82c850454a94d0dae1037d08c1773dc4b707b2b
straczi <k.arendt0302@gmail.com>
2025-07-25 23:21:43 +0200

    init

---
+++ src/main/java/dev/strocz/waterme/WatermeApplication.java
@@ -0,0 +1,13 @@
+package dev.strocz.waterme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatermeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatermeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/strocz/waterme/WatermeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.strocz.waterme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatermeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
